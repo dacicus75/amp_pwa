1afec4d707c883e8a814089b79cd0196a089f330
dacicus75 <dacicus75@hotmail.com>
2020-08-30 19:38:39 -0400

    added workbox-config file

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "globDirectory": "./",
+    "globPatterns": [
+      "img/**.*"
+    ],
+    "swSrc": "src/sw.js",
+    "swDest": "sw.js",
+    "globIgnores": [
+      "./workbox-config.js"
+    ]
+  };
